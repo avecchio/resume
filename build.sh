9607d0295af85e63df1caf968edf8c838671a88b
Austin Vecchio <au.vecchio@gmail.com>
2014-08-31 14:06:34 -0400

    Updated resume to reflect sophomore 2 semester. Remove all html from bash script since kali does not support htlatex

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ cp resume.tex html
 cp resume.tex pdf
 cd pdf
 pdflatex resume.tex
-cd ../html
-htlatex resume.tex
-cd ..
-rm html/resume.tex
+#cd ../html
+#htlatex resume.tex
+#cd ..
+#rm html/resume.tex
 rm pdf/resume.tex
 
